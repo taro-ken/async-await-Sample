d7aba785794d15d0f278b3bb6d85f9253c59606c
kentaro kimoto <exiler2411k@gmail.com>
2022-03-27 18:02:47 +0900

    init

---
+++ async-await-Sample/async-await-Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  async-await-Sample
+//
+//  Created by 木元健太郎 on 2022/03/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ async-await-Sample/async-await-SampleUITests/async_await_SampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  async_await_SampleUITestsLaunchTests.swift
+//  async-await-SampleUITests
+//
+//  Created by 木元健太郎 on 2022/03/27.
+//
+
+import XCTest
+
+class async_await_SampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
